c1af39d34de38de1828d330f4ffb562bf37d7512
Rahma Fadillah <rahmafadillah113@gmail.com>
2026-04-24 14:08:15 +0700

    chirper-laravel

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,16 +8,17 @@
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600,700" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
     <link href="https://cdn.jsdelivr.net/npm/daisyui@5/themes.css" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body class="min-h-screen flex flex-col bg-base-200 font-sans">
-    <nav class="navbar bg-base-100">
+<body class="min-h-screen flex flex-col font-sans bg-[#232946]">
+    <nav class="navbar bg-[#121629]">
         <div class="navbar-start">
-            <a href="/" class="btn btn-ghost text-xl">🐦 Chirper</a>
+            <a href="/" class="btn btn-ghost text-2xl font-bold font-['Quicksand'] hover:bg-transparent hover:bg-transparent hover:border-transparent">✨<span class="bg-gradient-to-r from-[#eebbc3] to-[#b8c1ec] bg-clip-text text-transparent">Chirper</span></a>
         </div>
         <div class="navbar-end gap-2">
-            <a href="#" class="btn btn-ghost btn-sm">Sign In</a>
-            <a href="#" class="btn btn-primary btn-sm">Sign Up</a>
+            <a href="#" class="btn btn-ghost btn-sm bg-[#eebbc3] text-[#232946] hover:bg-[#232946] hover:text-[#eebbc3] transition-all duration-300 active:translate-y-0.5">Sign In</a> 
+            <a href="#" class="btn btn-primary btn-sm bg-[#232946]/25 text-[#eebbc3] hover:bg-[#b8c1ec]/40 hover:text-[#eebbc3] transition-all duration-300 border border-[#ffffff] active:translate-y-0.5">Sign Up</a>
         </div>
     </nav>
 
@@ -25,9 +26,9 @@
         {{ $slot }}
     </main>
 
-    <footer class="footer footer-center p-5 bg-base-300 text-base-content text-xs">
+    <footer class="footer footer-center p-5 bg-[#121629] text-base-content text-xs text-[#fffffe]">
         <div>
-            <p>© {{ date('Y') }} Chirper - Built with Laravel and ❤️ by <span class="font-bold text-[#29327d]">Rahma Fadillah </span>(240170095)</php>
+            <p>© {{ date('Y') }} Chirper - Built with Laravel and 💙 by <span class="font-bold text-[#fffffe] font-['quicksand']">Rahma Fadillah </span>(240170095)</php>
         </div>
     </footer>
 </body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,15 +3,13 @@
             Welcome
         </x-slot:tittle>
         <div class="max-w-2xl mx-auto">
-            <div class="card bg-base-100 shadow mt-8">
+            <div class="card shadow-xl mt-8 backdrop-blur-lg bg-[#b8c1ec]/25 rounded-2xl border-2 border-[#121629]">
                 <div class="card-body">
                     <div>
-                        <h1 class="text-3xl font-bold">Welcome to Chirper!</h1>
-                        <p class="mt-4 text-base-content/60">This is your brand new Laravel application.
-Time to make it                            
-                        sing (or chirp)!</p>
+                        <h1 class="text-4xl font-bold text-[#fffffe]">Welcome to Chirper!</h1>
+                        <p class="mt-4 text-justify text-[#b8c1ec] font-['Quicksand']">This is your brand new Laravel application. Time to make it sing (or chirp) ! <br><br>Chirper adalah sebuah platform mikroblogging yang ramah, dimana pengguna dapat memposting pesan singkat yang disebut "chirps", melihat apa yang dikatakan orang lain, dan berinteraksi secara real time.</p>
                     </div>
                 </div>
             </div>
         </div>
-    </x-layout>
+</x-layout>
