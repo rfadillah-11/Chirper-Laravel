de9aec58d4edd66112625a70efc6f9adced0a19f
Rahma Fadillah <rahmafadillah113@gmail.com>
2026-04-27 17:44:18 +0700

    chirper-laravel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,12 +4,12 @@
     </x-slot :title>
     <div class="max-w-2xl mx-auto">
         @foreach ($chirps as $chirp)
-            <div class="card shadow-xl mt-8 backdrop-blur-lg bg-[#b8c1ec]/25 rounded-2xl border-2 border-[#121629]">
+            <div class="card shadow-xl mt-8 backdrop-blur-lg bg-[#eebbc3]/25 rounded-2xl border-2 border-[#121629]">
                 <div class="card-body">
                     <div>
-                        <div class="text-justify text-[#b8c1ec] font-semibold font-['Quicksand']">{{ $chirp['author' ] }}</div>
-                        <div class="mt-1 text-[#b8c1ec] font-['Quicksand']">{{ $chirp['message' ] }}</div>
-                        <div class="mt-2 text-justify text-[#b8c1ec] font-['Quicksand']">{{ $chirp['time' ] }}</div>
+                        <div class="text-justify text-[#232946] font-bold font-['Quicksand']">{{ $chirp['author' ] }}</div>
+                        <div class="mt-1 text-[#232946] font-['Quicksand']">{{ $chirp['message' ] }}</div>
+                        <div class="mt-2 text-justify text-[#232946]">{{ $chirp['time' ] }}</div>
                     </div>
                 </div>
             </div>
