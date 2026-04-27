a4c3275c19bb2b9164da72c8b4129d868ae77ba0
Rahma Fadillah <rahmafadillah113@gmail.com>
2026-04-27 15:35:11 +0700

    chirper-laravel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,9 @@
                 <div class="card-body">
                     <div>
                         <h1 class="text-4xl font-bold text-[#fffffe]">Welcome to Chirper!</h1>
-                        <p class="mt-4 text-justify text-[#b8c1ec] font-['Quicksand']">This is your brand new Laravel application. Time to make it sing (or chirp) ! <br><br>Chirper adalah sebuah platform mikroblogging yang ramah, dimana pengguna dapat memposting pesan singkat yang disebut "chirps", melihat apa yang dikatakan orang lain, dan berinteraksi secara real time.</p>
+                        <p>This is your brand new Laravel application. Time to make it sing (or chirp) ! </p>
+                        <p class="mt-4 text-justify text-[#b8c1ec] font-['Quicksand']">Chirper adalah sebuah platform mikroblogging yang ramah, dimana pengguna dapat memposting pesan singkat yang disebut "chirps", melihat apa yang dikatakan orang lain, dan berinteraksi secara real time.</p>
+
                     </div>
                 </div>
             </div>
