afe6fdffeb3a38b052697b428fd7c8b420504c45
Rahma Fadillah <rahmafadillah113@gmail.com>
2026-04-27 16:49:19 +0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ChirpController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ChirpController::class, 'index']);
+
