c8b400c1c239ef545f0b0d0f9f29a59a0bd005a9
Rahma Fadillah <rahmafadillah113@gmail.com>
2026-04-27 16:37:59 +0700

    chirper-laravel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,18 @@
 <x-layout>
-        <x-slot:tittle>
-            Welcome
-        </x-slot:tittle>
-        <div class="max-w-2xl mx-auto">
-            <div class="card shadow-xl mt-8 backdrop-blur-lg bg-[#b8c1ec]/25 rounded-2xl border-2 border-[#121629]">
+    <x-slot:title>
+        Welcome
+    </x-slot :title>
+    <div class="max-w-2xl mx-auto">
+        @foreach ($chirps as $chirp)
+            <div class="card bg-base-100 shadow mt-8">
                 <div class="card-body">
                     <div>
-                        <h1 class="text-4xl font-bold text-[#fffffe]">Welcome to Chirper!</h1>
-                        <p class="mt-4 text-justify text-[#b8c1ec] font-['Quicksand']">This is your brand new Laravel application. Time to make it sing (or chirp) ! </p>
-                        <p class="mt-4 text-justify text-[#b8c1ec] font-['Quicksand']">Now this is live on the internet! 🎉 <br>Chirper adalah sebuah platform mikroblogging yang ramah, dimana pengguna dapat memposting pesan singkat yang disebut "chirps", melihat apa yang dikatakan orang lain, dan berinteraksi secara real time.</p>
-
+                        <div class="font-semibold">{{ $chirp['author' ] }}</div>
+                        <div class="mt-1">{{ $chirp['message' ] }}</div>
+                        <div class="text-sm text-gray-500 mt-2">{{ $chirp['time' ] }}</div>
                     </div>
                 </div>
             </div>
-        </div>
+        @endforeach
+    </div>
 </x-layout>
